24845ae40da3004881615e0d58b01524c206594e
UJJWAL-379 <ujjwaljojy9447@gmail.com>
2026-08-29 13:18:19 +0530

    Bump service worker cache after bug fixes

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE='onam-odyssey-v4';
+const CACHE='onam-odyssey-v5';
 const ASSETS=['./','./index.html','./css/style.css','./js/game.js','./js/player.js','./js/particles.js','./js/pool.js','./js/audio.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('onam-odyssey-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET')return;
-  event.respondWith(fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-    return response;
-  }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+ if(event.request.method!=='GET')return;
+ event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
 });
