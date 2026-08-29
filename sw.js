76c21a487418bc24e5d763adf01e9333a327c0a2
UJJWAL-379 <ujjwaljojy9447@gmail.com>
2026-08-29 13:15:07 +0530

    Fix stale service worker cache causing game to load old JavaScript

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-const CACHE='onam-odyssey-v3';const ASSETS=['./','./index.html','./css/style.css','./js/game.js','./js/player.js','./js/particles.js','./js/pool.js','./js/audio.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const CACHE='onam-odyssey-v4';
+const ASSETS=['./','./index.html','./css/style.css','./js/game.js','./js/player.js','./js/particles.js','./js/pool.js','./js/audio.js','./manifest.json','./icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+    return response;
+  }).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+});
